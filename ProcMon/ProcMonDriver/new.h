d73b2478c125823203d9efcb0a0b4d210a74797a
Omer Katz <omerk2511@gmail.com>
2020-04-17 22:48:44 +0300

    Updated the new operator to expect nothing about its arguments.

--- ProcMon/ProcMonDriver/new.h
+++ ProcMon/ProcMonDriver/new.h
@@ -4,7 +4,7 @@
 
 #include "config.h"
 
-void* __cdecl operator new(size_t size, POOL_TYPE pool_type = PagedPool, ULONG tag = config::kDriverTag)
+void* __cdecl operator new(size_t size, POOL_TYPE pool_type, ULONG tag = 0)
 {
     auto address = tag ?
         ::ExAllocatePoolWithTag(pool_type, size, tag) :
